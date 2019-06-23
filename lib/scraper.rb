4590626a7dfd3471ab6e1de315b7514483820d25
Adella <adellakenn@gmail.com>
2019-06-23 02:17:30 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,17 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
+  #index_url = "./fixtures/student-site/index.html"
+  url = open(index_url)
+  doc = Nokogiri.HTML(url)
 
   def self.scrape_index_page(index_url)
-    
+    # binding.pry
+    :name => index_url.css(".student-name").text
+    :location => index_url.css(".studen-location").text
+    :profile_url => index_url.css(".view-profile-text").text
   end
 
   def self.scrape_profile_page(profile_url)
