1a03f498010fd304c65759139fb5816b6290d133
Adella <adellakenn@gmail.com>
2019-06-23 14:32:49 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,19 +4,49 @@ require 'nokogiri'
 
 class Scraper
   #index_url = "./fixtures/student-site/index.html"
-  url = open(index_url)
-  doc = Nokogiri.HTML(url)
-
+  
   def self.scrape_index_page(index_url)
-    # binding.pry
-    :name => index_url.css(".student-name").text
-    :location => index_url.css(".studen-location").text
-    :profile_url => index_url.css(".view-profile-text").text
+    # url = open(index_url)
+    doc = Nokogiri::HTML(open(index_url))
+    student_list = doc.css(".student-card")
+    students = []                             # wants an array
+  
+    student_list.map do |student|
+      students << {
+        :name => student.css("h4.student-name").text,
+        :location => student.css(".student-location").text,
+        :profile_url => student.css("a").attr("href").value
+      } 
+    end
+    students
   end
 
+# profile_url == "./fixtures/student-site/students/joe-burgess.html"
+# Not everyone will not have a twitter or some other social link
+# Needs twitter, linkedin, github, blog, profile_quote, bio 
   def self.scrape_profile_page(profile_url)
+    # binding.pry
+    # url = open(profile_url)
+    doc = Nokogiri::HTML(open(profile_url))
     
+    profile_quote = doc.css(".profile-quote").text 
+    bio = doc.css(".bio-content .description-holder p").text.strip 
+    students = {:bio => bio, :profile_quote => profile_quote}                             # wants a hash
+    
+    student_profile = doc.css("div.social-icon-container a")
+    student_profile.map do |student|
+       info_link = student.attr("href")
+        if info_link.include?("twitter")
+          students[:twitter] = info_link
+        elsif info_link.include?("linkedin")
+          students[:linkedin] = info_link
+        elsif info_link.include?("github")
+          students[:github] = info_link
+        else info_link.include?("blog")
+          students[:blog] = info_link
+        end
+      end
+       students
   end
-
 end
 
--- lib/student.rb
+++ lib/student.rb
@@ -4,20 +4,28 @@ class Student
 
   @@all = []
 
-  def initialize(student_hash)
+  def initialize(student_hash)  # {:name=>"Alex Patriquin", :location=>"New York, NY"}
+   
+    @name = student_hash[:name]
+    @location = student_hash[:location]
     
+    @@all << self
   end
 
-  def self.create_from_collection(students_array)
-    
+  def self.create_from_collection(students_array) # students_array == [{:name , :location}]
+    # binding.pry
+    students_array.each {|student| Student.new(student)}
   end
 
   def add_student_attributes(attributes_hash)
-    
+    # binding.pry
+    attributes_hash.each_key do |key|
+      self.send("#{key}=", attributes_hash[key])
+    end 
   end
 
   def self.all
-    
+    @@all 
   end
 end
 
